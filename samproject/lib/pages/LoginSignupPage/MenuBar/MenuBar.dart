cd7ee4fcd9fb5966c04cc032383902ea2e107b76
github_username <hamidreza.azarbad77@gmail.com>
2020-10-22 23:57:33 +0330

    create MenuBar for LogInPage

---
+++ samproject/lib/pages/LoginSignupPage/MenuBar/MenuBar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MenuBar extends StatefulWidget {
+  @override
+  _MenuBarState createState() => _MenuBarState();
+}
+
+class _MenuBarState extends State<MenuBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
