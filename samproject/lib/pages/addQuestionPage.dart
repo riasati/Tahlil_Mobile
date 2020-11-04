8f12249041ebf16603943949fa266d85ff73ceb9
Mohammad mahdi Soori <mohammadmahdisoori@gmail.com>
2020-11-04 14:51:56 +0330

    Add Kind of Question not complete!

--- samproject/lib/pages/addQuestionPage.dart
+++ samproject/lib/pages/addQuestionPage.dart
@@ -9,34 +9,356 @@ class AddQuestionPage extends StatefulWidget {
 
 class _AddQuestionPageState extends State<AddQuestionPage> {
   TextEditingController QuestionTextController = new TextEditingController();
-  FocusNode _focusNodeQuestionText = FocusNode();
+  TextEditingController TashrihiTextController = new TextEditingController();
 
-  // bool hasfocus = true;
-  // void _focusChange(bool isChange)
-  // {
-  //   print(isChange);
-  //   if(isChange)
-  //   {
-  //     setState(() {
-  //       hasfocus = true;
-  //     });
-  //   }
-  //   else
-  //     {
-  //       setState(() {
-  //         hasfocus = false;
-  //       });
-  //     }
-  //
-  // }
-
-   bool showQuestionTextForm = true;
-  void _showQuestionTextForm()
+  bool focusOnTashrihi = true;
+  void _focusChangeOnTashrihi(bool isChange)
   {
+    if(isChange)
+    {
       setState(() {
-        showQuestionTextForm = true;
+        focusOnTashrihi = true;
       });
+    }
+    else
+      {
+        setState(() {
+          focusOnTashrihi = false;
+        });
+      }
   }
+
+  bool focusOnQuestionText = true;
+  void _focusChangeOnQuestionText(bool isChange)
+  {
+    if(isChange)
+    {
+      setState(() {
+        focusOnQuestionText = true;
+      });
+    }
+    else
+    {
+      setState(() {
+        focusOnQuestionText = false;
+      });
+    }
+  }
+  Widget tashrihiWidget()
+  {
+    return Row(
+      textDirection: TextDirection.rtl,
+      children: [
+        Expanded(
+          flex: 1,
+          child: Text("پاسخ سوال",textDirection: TextDirection.rtl,),
+        ),
+        Expanded(
+          flex: 5,
+          child: InkWell(
+             child: (focusOnTashrihi) ? TextFormField(
+               textDirection: TextDirection.rtl,
+               controller: TashrihiTextController,
+               keyboardType: TextInputType.multiline,
+               maxLines: 3,
+               decoration: InputDecoration(border: OutlineInputBorder()),
+             )
+                :TextFormField(
+                textDirection: TextDirection.rtl,
+                controller: TashrihiTextController,
+                keyboardType: TextInputType.multiline,
+                maxLines: 3,
+                readOnly: true,
+                decoration: InputDecoration(border: InputBorder.none),
+             ),
+             onFocusChange: (value) => _focusChangeOnTashrihi(value),
+           )
+        ),
+      ],
+    );
+  }
+  String difficulty;
+  void onSelectedDifficultyMenu(int value)
+  {
+    if (value == 1) {
+      setState(() {
+        difficulty = "آسان";
+      });
+    }
+    else if (value == 2) {
+      setState(() {
+        difficulty = "متوسط";
+      });
+    }
+    else if (value == 3) {
+      setState(() {
+        difficulty = "سخت";
+      });
+    }
+  }
+  var difficultyList = [
+    PopupMenuItem(
+        value: 1,
+        child: Container(alignment: Alignment.centerRight,child: Text('آسان',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 2,
+        child: Container(alignment: Alignment.centerRight,child: Text('متوسط',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 3,
+        child: Container(alignment: Alignment.centerRight,child: Text('سخت',textDirection: TextDirection.rtl,))
+    ),
+  ];
+  int whichKind = 0;
+  String kind;
+  void onSelectedKindMenu(int value)
+  {
+    if (value == 1) {
+      setState(() {
+        kind = "تستی";
+        whichKind = 1;
+      });
+    }
+    else if (value == 2) {
+      setState(() {
+        kind = "جایخالی";
+        whichKind = 2;
+      });
+    }
+    else if (value == 3) {
+      setState(() {
+        kind = "چندگزینه ای";
+        whichKind = 3;
+      });
+    }
+    else if (value == 4) {
+      setState(() {
+        kind = "تشریحی";
+        whichKind = 4;
+      });
+    }
+  }
+  var kindList = [
+    PopupMenuItem(
+        value: 1,
+        child: Container(alignment: Alignment.centerRight,child: Text('تستی',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 2,
+        child: Container(alignment: Alignment.centerRight,child: Text('جایخالی',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 3,
+        child: Container(alignment: Alignment.centerRight,child: Text('چندگزینه ای',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 4,
+        child: Container(alignment: Alignment.centerRight,child: Text('تشریحی',textDirection: TextDirection.rtl,))
+    ),
+  ];
+  String chapter;
+  void onSelectedChapterMenu(int value)
+  {
+    if (value == 1) {
+      setState(() {
+        chapter = "فصل اول";
+      });
+    }
+    else if (value == 2) {
+      setState(() {
+        chapter = "فصل دوم";
+      });
+    }
+    else if (value == 3) {
+      setState(() {
+        chapter = "فصل سوم";
+      });
+    }
+    else if (value == 4) {
+      setState(() {
+        chapter = "فصل چهارم";
+      });
+    }
+    else if (value == 5) {
+      setState(() {
+        chapter = "فصل پنجم";
+      });
+    }
+    else if (value == 6) {
+      setState(() {
+        chapter = "فصل ششم";
+      });
+    }
+    else if (value == 7) {
+      setState(() {
+        chapter = "فصل هفتم";
+      });
+    }
+    else if (value == 8) {
+      setState(() {
+        chapter = "فصل هشتم";
+      });
+    }
+    else if (value == 9) {
+      setState(() {
+        chapter = "فصل نهم";
+      });
+    }
+    else if (value == 10) {
+      setState(() {
+        chapter = "فصل دهم";
+      });
+    }
+    else
+    {
+      setState(() {
+        chapter = null;
+      });
+    }
+  }
+  var chapterList = [
+    PopupMenuItem(
+        value: 1,
+        child: Container(alignment: Alignment.centerRight,child: Text('فصل اول',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 2,
+        child: Container(alignment: Alignment.centerRight,child: Text('فصل دوم',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 3,
+        child: Container(alignment: Alignment.centerRight,child: Text('فصل سوم',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 4,
+        child: Container(alignment: Alignment.centerRight,child: Text('فصل چهارم',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 5,
+        child: Container(alignment: Alignment.centerRight,child: Text('فصل پنجم',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 6,
+        child: Container(alignment: Alignment.centerRight,child: Text('فصل ششم',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 7,
+        child: Container(alignment: Alignment.centerRight,child: Text('فصل هفتم',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 8,
+        child: Container(alignment: Alignment.centerRight,child: Text('فصل هشتم',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 9,
+        child: Container(alignment: Alignment.centerRight,child: Text('فصل نهم',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 10,
+        child: Container(alignment: Alignment.centerRight,child: Text('فصل دهم',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 11,
+        child: Container(alignment: Alignment.centerRight,child: Text('هیچکدام',textDirection: TextDirection.rtl,))
+    ),
+  ];
+  String book;
+  void onSelectedBookMenu(int value)
+  {
+    if (value == 1) {
+      setState(() {
+        book = "ریاضی";
+      });
+    }
+    else if (value == 2) {
+      setState(() {
+        book = "فیزیک";
+      });
+    }
+    else if (value == 3) {
+      setState(() {
+        book = "شیمی";
+      });
+    }
+    else if (value == 4) {
+      setState(() {
+        book = "زیست";
+      });
+    }
+    else
+    {
+      setState(() {
+        book = null;
+      });
+    }
+  }
+  var bookList = [
+    PopupMenuItem(
+        value: 1,
+        child: Container(alignment: Alignment.centerRight,child: Text('ریاضی',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 2,
+        child: Container(alignment: Alignment.centerRight,child: Text('فیزیک',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 3,
+        child: Container(alignment: Alignment.centerRight,child: Text('شیمی',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 4,
+        child: Container(alignment: Alignment.centerRight,child: Text('زیست',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 5,
+        child: Container(alignment: Alignment.centerRight,child: Text('هیچکدام',textDirection: TextDirection.rtl,))
+    ),
+  ];
+  String paye;
+  void onSelectedPayeMenu(int value)
+  {
+    if (value == 1) {
+      setState(() {
+        paye = "دهم";
+      });
+    }
+    else if (value == 2) {
+      setState(() {
+        paye = "یازدهم";
+      });
+    }
+    else if (value == 3) {
+      setState(() {
+        paye = "دوازدهم";
+      });
+    }
+    else
+      {
+        setState(() {
+          paye = null;
+        });
+      }
+  }
+  var payeList = [
+    PopupMenuItem(
+        value: 1,
+        child: Container(alignment: Alignment.centerRight,child: Text('دهم',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 2,
+        child: Container(alignment: Alignment.centerRight,child: Text('یازدهم',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 3,
+        child: Container(alignment: Alignment.centerRight,child: Text('دوازدهم',textDirection: TextDirection.rtl,))
+    ),
+    PopupMenuItem(
+        value: 4,
+        child: Container(alignment: Alignment.centerRight,child: Text('هیچکدام',textDirection: TextDirection.rtl,))
+    ),
+  ];
+
   void _deleteImage(bool ImageOne)
   {
     setState(() {
@@ -46,32 +368,7 @@ class _AddQuestionPageState extends State<AddQuestionPage> {
         _ImageTwo = null;
     });
   }
-  @override
-  void initState() {
-    super.initState();
-  //  _focusHandling(_focusNode,showTextForm);
-    _focusNodeQuestionText.addListener(() {
-      //print("Has focus: ${_focusNodeQuestionText.hasFocus}");
-      if (_focusNodeQuestionText.hasFocus)
-      {
-        setState(() {
-          showQuestionTextForm = true;
-        });
-      }
-      else
-        {
-          setState(() {
-            showQuestionTextForm = false;
-          });
-        }
-    });
-  }
 
-  @override
-  void dispose() {
-    _focusNodeQuestionText.dispose();
-    super.dispose();
-  }
   File _ImageOne;
   File _ImageTwo;
 
@@ -142,28 +439,29 @@ class _AddQuestionPageState extends State<AddQuestionPage> {
                     ),
                     Expanded(
                       flex: 5,
-                      child: (showQuestionTextForm) ? TextFormField(
-                        autofocus: true,
-                        textDirection: TextDirection.rtl,
-                        controller: QuestionTextController,
-                        keyboardType: TextInputType.multiline,
-                        maxLines: 3,
-                          focusNode: _focusNodeQuestionText,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                          )
+                      child: InkWell(
+                        child: (focusOnQuestionText) ? TextFormField(
+                          autofocus: true,
+                          textDirection: TextDirection.rtl,
+                          controller: QuestionTextController,
+                          keyboardType: TextInputType.multiline,
+                          maxLines: 3,
+                          decoration: InputDecoration(border: OutlineInputBorder()),
+                        )
+                            :TextFormField(
+                          textDirection: TextDirection.rtl,
+                          controller: QuestionTextController,
+                          keyboardType: TextInputType.multiline,
+                          maxLines: 3,
+                          readOnly: true,
+                          decoration: InputDecoration(border: InputBorder.none),
+                        ),
+                        onFocusChange: (value) => _focusChangeOnQuestionText(value),
                       )
-                          : InkWell(
-                        onTap: () => _showQuestionTextForm() ,
-                            child: Container(
-                        //      decoration: BoxShape.rectangle(),
-                        child: Text(QuestionTextController.text,textDirection: TextDirection.rtl,maxLines: 3),
-                      ),
-                          ) ,
                     ),
                   ],
                 ),
-                TextFormField(),
+                //TextFormField(),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
@@ -174,10 +472,67 @@ class _AddQuestionPageState extends State<AddQuestionPage> {
                         : Container(),
                   ],
                 ),
-                // InkWell(
-                //   child: (hasfocus) ? TextFormField(controller: QuestionTextController,decoration: InputDecoration(border: OutlineInputBorder()),):  TextFormField(controller: QuestionTextController,readOnly: true,decoration: InputDecoration(border: InputBorder.none),),
-                //   onFocusChange: (value) => _focusChange(value),
-                // )
+                Row(
+                  textDirection: TextDirection.rtl,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: PopupMenuButton(
+                        //padding: EdgeInsets.all(50.0),
+                        child: (paye == null) ? Text("پایه تحصیلی") : Text(paye),
+                        onSelected: onSelectedPayeMenu,
+                        itemBuilder: (context) => payeList,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: PopupMenuButton(
+                        //padding: EdgeInsets.all(50.0),
+                        child: (book == null) ? Text("درس") : Text(book),
+                        onSelected: onSelectedBookMenu,
+                        itemBuilder: (context) => bookList,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: PopupMenuButton(
+                        //padding: EdgeInsets.all(50.0),
+                        child: (chapter == null) ? Text("فصل") : Text(chapter),
+                        onSelected: onSelectedChapterMenu,
+                        itemBuilder: (context) => chapterList,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  textDirection: TextDirection.rtl,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: PopupMenuButton(
+                        //padding: EdgeInsets.all(50.0),
+                        child: (kind == null) ? Text("نوع سوال") : Text(kind),
+                        onSelected: onSelectedKindMenu,
+                        itemBuilder: (context) => kindList,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: PopupMenuButton(
+                        //padding: EdgeInsets.all(50.0),
+                        child: (difficulty == null) ? Text("دشواری سوال") : Text(difficulty),
+                        onSelected: onSelectedDifficultyMenu,
+                        itemBuilder: (context) => difficultyList,
+                      ),
+                    ),
+                  ],
+                ),
+                if (whichKind == 1) Container()
+                else if (whichKind == 2) Container()
+                else if (whichKind == 3) Container()
+                else if (whichKind == 4) tashrihiWidget(),
 
               ],
             ),
