f276d6bd9c0327627fd96856ae6c17797e367ecb
github_username <hamidreza.azarbad77@gmail.com>
2020-12-07 12:41:56 +0330

    Create bottom navigator

---
+++ samproject/lib/pages/TakeExamPage/main.dart
@@ -0,0 +1,13 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:scroll_snap_list/scroll_snap_list.dart';
+
+import 'BottomNavigator.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    bottomNavigationBar: BottomNavigator(),
+  ),
+));
