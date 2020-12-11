97cf10994ea250739f12a1f08fa544fd902b1804
Mohammad mahdi Soori <mohammadmahdisoori@gmail.com>
2020-12-11 13:16:36 +0330

    Create Timer

--- samproject/lib/pages/TakeExamPage/main.dart
+++ samproject/lib/pages/TakeExamPage/main.dart
@@ -2,12 +2,24 @@ import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:samproject/pages/TakeExamPage/timer.dart';
 import 'package:scroll_snap_list/scroll_snap_list.dart';
 
 import 'BottomNavigator.dart';
+//TimerWidget(StartUserExam: new DateTime.now(),ExamLenght: 30,EndExam: DateTime.now().add(Duration(minutes: 40)),EndTimerFunction: (){print("hello");},),
+//TimerWidget(StartUserExam: new DateTime.now(),ExamLenght: 30,EndExam: DateTime.now().add(Duration(minutes: 20)),EndTimerFunction: (){print("hello");},),
+//TimerWidget(StartUserExam: new DateTime.now(),ExamLenght: 1,EndExam: DateTime.now().add(Duration(minutes: 2)),EndTimerFunction: (){print("hello");},),
 
 void main() => runApp(MaterialApp(
   home: Scaffold(
+    // body: Column(
+    //   children: [
+    //     Container(height: 50,child: Text("asdfads"),),
+    //     TimerWidget(StartUserExam: new DateTime.now(),ExamLenght: 30,EndExam: DateTime.now().add(Duration(minutes: 40)),EndTimerFunction: (){print("hello");},),
+    //     TimerWidget(StartUserExam: new DateTime.now(),ExamLenght: 30,EndExam: DateTime.now().add(Duration(minutes: 20)),EndTimerFunction: (){print("hello");},),
+    //     TimerWidget(StartUserExam: new DateTime.now(),ExamLenght: 1,EndExam: DateTime.now().add(Duration(minutes: 2)),EndTimerFunction: (){print("hello");},),
+    //   ],
+    // ),
     bottomNavigationBar: BottomNavigator(),
   ),
 ));
--- samproject/lib/pages/TakeExamPage/timer.dart
+++ samproject/lib/pages/TakeExamPage/timer.dart
@@ -1,1 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_countdown_timer/flutter_countdown_timer.dart';
+
+class TimerWidget extends StatefulWidget {
+  DateTime StartUserExam;
+  int ExamLenght;
+  DateTime EndExam;
+  VoidCallback EndTimerFunction;
+  TimerWidget({Key key, this.StartUserExam,this.EndExam,this.ExamLenght,this.EndTimerFunction = null}) : super(key: key);
+  @override
+  _TimerWidgetState createState() => _TimerWidgetState();
+}
+
+class _TimerWidgetState extends State<TimerWidget> {
+  int endTime;
+  @override
+  void initState() {
+
+    super.initState();
+    if (widget.StartUserExam.add(Duration(minutes: widget.ExamLenght)).isBefore(widget.EndExam))
+    {
+      endTime = widget.StartUserExam.add(Duration(minutes: widget.ExamLenght)).millisecondsSinceEpoch;
+    }
+    else
+      {
+        endTime = widget.EndExam.millisecondsSinceEpoch;
+      }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return CountdownTimer(endTime: endTime,onEnd: widget.EndTimerFunction,);
+  }
+}
